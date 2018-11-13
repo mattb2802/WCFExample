6f524645719eab556dbbf329c0b743daef627155
boultonm <matthew.boulton@bydeluxe.com>
2018-11-13 12:22:27 -0800

    Initial Check in

---
+++ ServiceComponents/ServiceInstaller.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel;
+
+
+namespace WCFExample.ServiceComponents
+{
+    [RunInstaller(true)]
+    public class WcfExampleServiceInstaller : CommonInstaller
+    {
+        public WcfExampleServiceInstaller()
+        {
+            ServiceName = "WCFExampleService";
+            ServiceDescription = "WCF Example Windows Service";
+            AssemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            ConfigureInstaller();
+
+        }
+
+    }
+}
---
+++ ServiceComponents/WCFExampleHostService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using System.Configuration;
+using System.Diagnostics;
+
+using System.Net;
+
+using System.ServiceModel.Web;
+using System.ServiceProcess;
+
+
+namespace WCFExample.ServiceComponents
+{
+    public partial class WcfExampleHostService : ServiceBase
+    {
+        private const string HttpsBaseAddressFormat = "http://{0}:{1}/{2}";
+        private string _httpsBaseAddress;
+
+
+        public WcfExampleHostService()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Init();
+
+        }
+
+        public void Init()
+        {
+            try
+            {
+     
+                CalculatorRestFulService();
+              
+            }
+            catch (Exception ex)
+            {
+               
+                EventLog.WriteEntry("WCF Example Service failed to start. Error: " + ex.Message, EventLogEntryType.Error);
+                Environment.Exit(-1);
+            }
+
+        }
+
+
+
+        private void CalculatorRestFulService()
+        {
+            _httpsBaseAddress = string.Format(HttpsBaseAddressFormat, Dns.GetHostName(), ConfigurationManager.AppSettings.Get("HttpsServicePort"), ConfigurationManager.AppSettings.Get("JsonUrl"));
+            Uri httpUrl = new Uri(_httpsBaseAddress);
+            WebServiceHost host = new WebServiceHost(typeof(CalculatorService), httpUrl);
+            host.Open();
+
+
+        }
+
+
+
+    }
+}
